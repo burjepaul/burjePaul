441b7ff782c8e6361d73c47f38262978c27dc053
burjepaul <burjepaul97@gmail.com>
2023-09-20 20:54:49 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import HomePage from "./pages/HomePage/homePage";
+
+function App() {
+  return (
+    <HomePage/>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage/homePage.jsx
@@ -0,0 +1,49 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, PerspectiveCamera, OrbitControls } from "@react-three/drei";
+import { motion } from "framer-motion-3d";
+
+import './homePage.styles.scss'
+import Phone3D from "../../components/3dComponents/MobilePhone";
+import Monitor3D from "../../components/3dComponents/Monitor";
+import Loader from "../../components/Loader/Loader";
+import Rendering from "../../components/3dComponents/3dRendering";
+
+
+const HomePage = () => {
+
+    return(
+      <div className="container">
+
+          <div className="preview">
+            <Canvas shadows gl={{preserveDrawingBuffer:true}}>
+              <Suspense fallback={<Loader/>}>
+
+                <hemisphereLight intensity={3} groundColor={"black"}/>
+                <ambientLight intensity={1} />
+                {/* <pointLight color="lightblue" intensity={20} position={[1, 1 ,7]}/> */}
+                <spotLight color="lightblue" intensity={10} position={[1,1,10]} angle={15} castShadow/>
+                <Phone3D/>
+                <Monitor3D/>
+                <Rendering/>
+                <OrbitControls
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2}
+                    minPolarAngle={Math.PI / 2}
+                    autoRotate
+                    />
+                <PerspectiveCamera makeDefault position={[0, 0, 12]}/>
+
+
+
+              </Suspense>
+            </Canvas>
+          </div>
+          
+          <div className="footer">
+          </div>
+      </div>        
+    )
+}
+
+export default HomePage
