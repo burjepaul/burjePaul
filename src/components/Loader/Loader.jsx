57b3bc698f72edc4f6389ffbcc5807951c5e9862
burjepaul <burjepaul97@gmail.com>
2023-11-07 20:33:18 +0200

    final touches

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,15 +5,20 @@ import './Loader.scss'
 const Loader = () => {
     const {progress} = useProgress()
 
+    console.log(progress)
+
     return(
         <Html>
             <span className="canvas-loader"></span>
                 {progress < 100 ? 
-                <div className="spinner">
-                    <svg>
-                        <use href={`${icons}#icon-loader`}></use>
-                    </svg>
-            </div> : null}
+                    <div className="spinner">
+                        <svg>
+                            <use href={`${icons}#icon-loader`}></use>
+                        </svg>
+                    </div> 
+                : 
+                    null
+                }
                 <h2>{progress.toFixed(2)}%</h2>
         </Html>
     )
